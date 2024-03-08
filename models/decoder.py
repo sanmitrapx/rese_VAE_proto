061ab5fb6cb8f6f7d6e9fb10f912263f8d7c370b
Michalis Michaelides <michalis.michaelides@physicsx.ai>
2024-03-08 13:22:01 +0000

    Lint.

--- models/decoder.py
+++ models/decoder.py
@@ -2,35 +2,43 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+
 class ReshapeRightmost(nn.Module):
     """
     Helper layer to reshape the rightmost dimension of a tensor.
 
     This can be used as a component of nn.Sequential.
     """
+
     def __init__(self, shape):
         """
         Args:
             shape: desired rightmost shape
-        """        
+        """
         super().__init__()
         self._shape = shape
+
     def forward(self, input):
         # reshapes the last dimension into self.shape
         return input.reshape(input.shape[:-1] + self._shape)
-        
+
+
 class Decoder(nn.Module):
     """
-    Map the latent variable z to a tensor with shape 
+    Map the latent variable z to a tensor with shape
     [Batch, num_channels, width, height]
     using a FFNN with 2 hidden layers.
     """
-    def __init__(self, z_dim: int, 
-            hidden_dim: int, 
-            num_channels: int=1, 
-            width: int=64, 
-            height: int=64, 
-            p_drop: int=0.1):
+
+    def __init__(
+        self,
+        z_dim: int,
+        hidden_dim: int,
+        num_channels: int = 1,
+        width: int = 64,
+        height: int = 64,
+        p_drop: float = 0.1,
+    ):
         super().__init__()
         """
         Args:
@@ -43,32 +51,32 @@ class Decoder(nn.Module):
             p_drop: dropout rate before linear layers
         """
         self.decoder = nn.Sequential(
-        nn.Dropout(p_drop),
-        nn.Linear(z_dim, hidden_dim),
-        nn.ReLU(),
-        nn.Dropout(p_drop),
-        nn.Linear(hidden_dim, hidden_dim),
-        nn.ReLU(),
-        nn.Dropout(p_drop),
-        nn.Linear(hidden_dim, num_channels * width * height),
-        ReshapeRightmost((num_channels, width, height)),
+            nn.Dropout(p_drop),
+            nn.Linear(z_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(p_drop),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(p_drop),
+            nn.Linear(hidden_dim, num_channels * width * height),
+            ReshapeRightmost((num_channels, width, height)),
         )
 
         self.width = width
         self.height = height
         self.num_channels = num_channels
 
-    def forward(self, z: torch.Tensor)-> torch.Tensor:
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
         """
         Define the forward computation on the latent z and
         return the parameter for the data (likelihood) distribution
-        each is of size [batch_size, num_channels, width, height] 
+        each is of size [batch_size, num_channels, width, height]
 
         Args:
             z: latent code
 
         Returns:
-            logits_img: decoder output parameterising 
+            logits_img: decoder output parameterising
                         the Bernouli liklihood
 
         """
--- models/encoder.py
+++ models/encoder.py
@@ -3,23 +3,29 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+
 class FlattenImage(nn.Module):
     def forward(self, input: torch.Tensor):
         return input.reshape(input.shape[:-3] + (-1,))
 
+
 class Encoder(nn.Module):
     """
-    Map the image of shape [Batch, num_channels, width, height] 
-    to a latent code (variable) z 
+    Map the image of shape [Batch, num_channels, width, height]
+    to a latent code (variable) z
     with shape [Batch, num_channels, width, height]
     using a FFNN with 2 hidden layers.
     """
-    def __init__(self, z_dim: int, 
-                hidden_dim: int, 
-                num_channels: int=1, 
-                width: int=64,
-                height: int=64, 
-                p_drop: float=0.1):
+
+    def __init__(
+        self,
+        z_dim: int,
+        hidden_dim: int,
+        num_channels: int = 1,
+        width: int = 64,
+        height: int = 64,
+        p_drop: float = 0.1,
+    ):
         super().__init__()
         """
         Args:
@@ -35,41 +41,32 @@ class Encoder(nn.Module):
 
         """
         self.encoder = nn.Sequential(
-        FlattenImage(),
-        nn.Dropout(p_drop),
-        nn.Linear(num_channels * width * height, hidden_dim//2),
-        nn.ReLU(),
-        nn.Dropout(p_drop),
-        nn.Linear(hidden_dim//2, hidden_dim//2),
-        nn.ReLU(),
-        nn.Dropout(p_drop),
-        nn.Linear(hidden_dim//2, hidden_dim)
-        )
-        self.locs = nn.Sequential(
+            FlattenImage(),
             nn.Dropout(p_drop),
-            nn.Linear(hidden_dim, z_dim)
-        )
-        self.scales = nn.Sequential(
+            nn.Linear(num_channels * width * height, hidden_dim // 2),
+            nn.ReLU(),
             nn.Dropout(p_drop),
-            nn.Linear(hidden_dim, z_dim)
-        )      
-          
+            nn.Linear(hidden_dim // 2, hidden_dim // 2),
+            nn.ReLU(),
+            nn.Dropout(p_drop),
+            nn.Linear(hidden_dim // 2, hidden_dim),
+        )
+        self.locs = nn.Sequential(nn.Dropout(p_drop), nn.Linear(hidden_dim, z_dim))
+        self.scales = nn.Sequential(nn.Dropout(p_drop), nn.Linear(hidden_dim, z_dim))
 
-    def forward(self, x: torch.Tensor)-> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """
-        Args: 
+        Args:
             x: a batch of images
 
         Returns:
             lambda_mu: encoded posterior mean
-            lambda_sigma: encoded posterior (positive) 
+            lambda_sigma: encoded posterior (positive)
                             square root covariance
         """
-        
+
         hidden = self.encoder(x)
-        
+
         lambda_mu = self.locs(hidden)
-        lambda_sigma = torch.exp(self.scales(hidden)) 
-        return lambda_mu, lambda_sigma            
-          
- 
+        lambda_sigma = torch.exp(self.scales(hidden))
+        return lambda_mu, lambda_sigma
